4ce3e17b81c81e799d373f9e2f5c3debc18164ba
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2022-12-27 17:40:33 +0100

    rework example for new structure

--- example.py
+++ example.py
@@ -3,33 +3,99 @@
 from thermalmodel.nodes import HeatStorageNode
 import thermalmodel.links as links
 
-# create  HSN
-Battery = HeatStorageNode({ 'mass': 10, 'heatCapacity': 20 })
-BoardComputer = HeatStorageNode({ 'mass': 5, 'heatCapacity': 8 })
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#                                   Create HSN
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-# attach IFN to HSN
-battery_global_interface_properties = {  'emissivity': 0.2, 'absorptivity': 0.6  }
-Battery.addInterfaceNode('top-face', { 'area': 12.4, **battery_global_interface_properties })
-Battery.addInterfaceNode('bottom-face', { 'area': 12.4, **battery_global_interface_properties })
-Battery.addInterfaceNode('front-face', { 'area': 3.8, **battery_global_interface_properties })
-
-BoardComputer.addInterfaceNode('bottom-face', { 'emissivity': 0.7, 'absorptivity': 0.2, 'area': 8.2 })
-
-# add links between IFNs
-BoardComputer.addInterfaceLink('bottom-face', 'BAT-top', Battery.interfaces['top-face'], {
-    'linkType': links.Manual({ 'viewingFactor': 10 }),
+Battery = HeatStorageNode({
+    'mass': 10,
+    'heatCapacity': 20,
+    'heatGeneration': 13.2,
+    'temperature': 20,
 })
-BoardComputer.addInterfaceLink('bottom-face', 'BAT-bottom', Battery.interfaces['bottom-face'], {
-    'linkType': links.Manual({ 'viewingFactor': 0 }),
-})
-BoardComputer.addInterfaceLink('bottom-face', 'BAT-front', Battery.interfaces['front-face'], {
-    'linkType': links.Manual({ 'viewingFactor': 0 }),
+BoardComputer = HeatStorageNode({
+    'mass': 5,
+    'heatCapacity': 8,
+    # heat generation is assumed 0 if not specified
+    'temperature': 21.2,
 })
 
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#                                Attach IFN to HSN
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+Battery.addInterfaceNode('top-face', {
+    'emissivity': 10,
+    'absorptivity': 0.4,
+})
+Battery.addInterfaceNode('bottom-face', {
+    'emissivity': 10,
+    'absorptivity': 0.4,
+})
+Battery.addInterfaceNode('front-face', {
+    'emissivity': 10,
+    'absorptivity': 0.4,
+})
+
+
+BoardComputer.addInterfaceNode('bottom-face', {
+    'emissivity': 10,
+    'absorptivity': 0.4,
+})
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#                             Add links between IFNs
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+BoardComputer.addInterfaceLink('BAT-top', 'bottom-face',
+                               Battery.interfaces['top-face'],
+                               [
+                                   links.RadiationLink,
+                                   links.ConductionLink,
+                                   links.ContactLink,
+                               ],
+                               {
+                                   'radiationArea1': 12.4,  # used by radiation calculations
+                                   'radiationArea2': 2.4,   # used by radiation calculations
+                                   'viewingFactor': 2.4,    # used by radiation calculations
+                                   'contactArea': 20,       # used by contact calculations
+                                   'resistance': 4.2,       # used by contact calculations
+                                   'conductionArea': 8.51,  # used by conduction calculations
+                                   'conductivity': 3.8,     # used by conduction calculations
+                                   'length': 0.4,           # used by conduction calculations
+                               }
+                               )
+
+BoardComputer.addInterfaceLink('BAT-bottom', 'bottom-face',
+                               Battery.interfaces['bottom-face'],
+                               [
+                                   links.ContactLink,
+                               ],
+                               {
+                                   'contactArea': 5.2,
+                                   'resistance': 4.2,
+                               }
+                               )
+
+BoardComputer.addInterfaceLink('BAT-front', 'bottom-face',
+                               Battery.interfaces['front-face'],
+                               [
+                                   links.ConductionLink,
+                               ],
+                               {
+                                   'conductionArea': 8.51,
+                                   'conductivity': 3.8,
+                                   'length': 0.4,
+                               }
+                               )
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#                                 Get temperature
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Get temperature of HSN at current time step
 # This would perform the computations behind the scenes
-BoardComputer.computeInterfaceTemperatures()
-BoardComputer.sumInterfaceTemperatures()
-BAT_TEMP = BoardComputer.getTemperature()
-print(BAT_TEMP)
+
+board_temperature = BoardComputer.computeTemperature()
+battery_temperature = Battery.computeTemperature()
+print(f'board_temperature = {board_temperature}')
+print(f'battery_temperature = {battery_temperature}')
